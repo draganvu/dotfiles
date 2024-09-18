522a2f3b1818c45a7f0663728c14213d60a5df9f
draganvu <dvujovic@protonmail.ch>
2024-09-18 01:01:37 +0100

    Added urxvt colors

--- deploy.sh
+++ deploy.sh
@@ -15,10 +15,15 @@ echo "Creating directories..."
 mkdir -p "$FOLDER_URXVT"
 mkdir -p "$FOLDER_COLORS"
 
-# Download files from Github
+# Download files from Github to directories
+echo "Downloading files..."
 wget -P "$FOLDER_URXVT" "$GITHUB_URL/.urxvt/ext/pasta"
 wget -P "$FOLDER_URXVT" "$GITHUB_URL/.urxvt/ext/resize-font"
-#wget -P "$FOLDER2" "$GITHUB_URL/.urxvt/ext/resize-font"
+wget -P "$FOLDER_COLORS" "$GITHUB_URL/.colors/zemun"
+
+# Download files from Github to ~/$HOME
+wget -P "$HOME" "$GITHUB_URL/.Xresources"
+wget -P "$HOME" "$GITHUB_URL/.gitconfig"
 
 xrdb ~/.Xresources
 
